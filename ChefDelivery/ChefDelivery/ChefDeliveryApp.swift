607373daded133f7a9e05bc8e018d810768ea99c
GiovannaDP <giovannadanellipau@gmail.com>
2023-12-07 22:43:32 -0300

    feat: creat swiftUI project

---
+++ ChefDelivery/ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Giovanna Danelli Pau on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
